183d744b728a861786ad6dbeb1b3c51e0c5ffd71
easonhan <nbkhic@qq.com>
2021-07-18 14:43:12 +0800

    first commit

---
+++ test_fib.py
@@ -0,0 +1,14 @@
+# 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377 ,610, 987……
+def fib_item(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fib_item(n - 1) + fib_item(n - 2)
+
+def test_fib_item():
+    assert fib_item(1) == 1
+    assert fib_item(2) == 1
+    assert fib_item(3) == 2
+    assert fib_item(4) == 3
+    assert fib_item(16) == 987
